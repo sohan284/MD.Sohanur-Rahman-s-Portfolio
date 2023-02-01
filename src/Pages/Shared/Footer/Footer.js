1ef31f1d375dce9f86a3a1dce601722b975dd335
srsohan284 <srsohan284.com>
2023-02-01 11:43:23 +0600

    up

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,14 +3,12 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='foot'>
-
             <div data-aos="zoom-in" className='flex items-center pt-5  justify-evenly lg:flex-row md:flex-row flex-col w-full'>
                 <div  className='text-accent '>
                 <h1 className='font-bold text-3xl'>MD.Sohanur Rahman Sohan</h1>
                     <h2 className='font-semibold text-lg'>Junior Web Developer</h2>
                 </div>
                 <div className='flex  flex-col justify-center'>
-                  
                     <div className='flex items-center'>
           <img width={35} src="https://i.ibb.co/5WR76z2/pngwing-com.png" alt="" />
           <p className='text-primary ml-2 text-lg font-bold'>+01788175088</p>
