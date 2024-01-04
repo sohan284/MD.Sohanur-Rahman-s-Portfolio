2ecc6ab64dc3d0e3c26f82fdcce51d2ffde93b32
Sohanur <sr.sohan088@outlook.com>
2024-01-04 13:17:42 +0600

    change image

--- src/Pages/Home/AboutMe.js
+++ src/Pages/Home/AboutMe.js
@@ -4,30 +4,30 @@ const AboutMe = () => {
     return (
         <div className='container mb-10 mx-auto'>
             <div data-aos="fade-up" className='  skills shadow-xl p-3 rounded-md'>
-                    <div className='flex justify-evenly mt-5 flex-col lg:flex-row  items-center'>
+                <div className='flex justify-evenly mt-5 flex-col lg:flex-row  items-center'>
                     <div data-aos="zoom-in-up">
-                            <img className='rounded-lg' width={220} src="https://i.ibb.co/8m1zNtv/Sohanur-Rahman.png" alt="" />
-                        </div>
-                        <div> <p className='text-accent text-xl m-5'>
-                          <p data-aos="fade-up"
-     data-aos-duration="100" className='font-bold text-3xl text-center'>MD.Sohanur Rahman Sohan 
-                              </p>
-                              <p data-aos="fade-up"
-     data-aos-duration="200" className='font-semibold text-xl text-center'>Department of Software Engineering </p><p data-aos="fade-up"
-     data-aos-duration="300" className='font-thin text-center text-lg'>
-                              (Expected Graduation Year, 2022)</p><p data-aos="fade-up"
-     data-aos-duration="400" className='font-semibold text-xl text-center '>Daffodil International University </p>
-                        </p></div>
-                     
+                        <img className='rounded-lg' width={220} src="https://i.ibb.co/wdKzct8/SrPhoto.jpg" alt="" />
                     </div>
+                    <div> <p className='text-accent text-xl m-5'>
+                        <p data-aos="fade-up"
+                            data-aos-duration="100" className='font-bold text-3xl text-center'>MD.Sohanur Rahman Sohan
+                        </p>
+                        <p data-aos="fade-up"
+                            data-aos-duration="200" className='font-semibold text-xl text-center'>Department of Software Engineering </p><p data-aos="fade-up"
+                                data-aos-duration="300" className='font-thin text-center text-lg'>
+                            (Expected Graduation Year, 2022)</p><p data-aos="fade-up"
+                                data-aos-duration="400" className='font-semibold text-xl text-center '>Daffodil International University </p>
+                    </p></div>
 
                 </div>
+
+            </div>
             <p data-aos="fade-up"
-     data-aos-duration="500" className='text-md p-3 text-accent mb-5'>I’am a junior front end developer. Usually I create applications with reactjs.My goal
+                data-aos-duration="500" className='text-md p-3 text-accent mb-5'>I’am a junior front end developer. Usually I create applications with reactjs.My goal
                 is to finish the task on time and trying to learn more. I want to be a full-stack developer.I love Programming,Developing,Learning and Problem Solving.I am passionate about taking risks and solving them by communiacating and sharing experience and knowledge. Currently I am pursuing Bachelor’s Degree in
                 Software Engineering.</p>
             <div className='grid grid-cols-1 gap-5'>
-                <div   className='border foot   p-3 rounded-md'>
+                <div className='border foot   p-3 rounded-md'>
                     <h1 data-aos="fade-up" data-aos-duration="600" className='text-4xl mb-2 text-center uppercase text-accent font-semibold '>Skills</h1> <hr />
                     <div className='ml-5 mt-5 flex justify-evenly flex-col lg:flex-row sm:flex-row'>
                         <div className='flex my-2'>
@@ -38,7 +38,7 @@ const AboutMe = () => {
                         </div>
                     </div>
                     <div className='ml-5 flex justify-evenly flex-col lg:flex-row sm:flex-row'>
-                        <div   className='flex my-2'>
+                        <div className='flex my-2'>
                             <img data-aos="fade-up" width={30} src="https://i.ibb.co/tHVGhsN/bootstrapicon.png" alt="" /><progress data-aos="fade-up" class="progress mt-3 progress-primary border h-3 ml-3 w-64" value="87" max="100"></progress>
                         </div>
                         <div className='flex my-2'>
@@ -54,7 +54,7 @@ const AboutMe = () => {
                         </div>
                     </div>
                     <div className='ml-5 flex justify-evenly flex-col lg:flex-row sm:flex-row'>
-                        <div   className='flex my-2'>
+                        <div className='flex my-2'>
                             <img data-aos="fade-up" width={30} src="https://i.ibb.co/HnZkrVj/kisspng-firebase-cloud-messaging-computer-icons-google-clo-bell-notification-youtube-transparent-amp.png" alt="" /><progress data-aos="fade-up" class="progress mt-3 progress-primary border h-3 ml-3 w-64" value="50" max="100"></progress>
                         </div>
                         <div data-os="zoom-in" className='flex my-2'>
@@ -62,15 +62,15 @@ const AboutMe = () => {
                         </div>
                     </div>
                     <div className='ml-5 flex justify-evenly flex-col lg:flex-row sm:flex-row'>
-                        <div   className='flex my-2'>
+                        <div className='flex my-2'>
                             <img data-aos="fade-up" width={30} src="https://i.ibb.co/H7TH3JY/kisspng-mongodb-database-clip-art-computer-icons-nosql-5b900df9c897c4-0301692015361674178216.png" alt="" /><progress data-aos="fade-up" class="progress mt-3 progress-primary border h-3 ml-3 w-64" value="30" max="100"></progress>
                         </div>
-                        <div   className='flex my-2'>
+                        <div className='flex my-2'>
                             <img data-aos="fade-up" width={30} className='rounded-full' src="https://i.ibb.co/Lk3sbNF/png-clipart-common-daisy-flower-saisy-text-logo-thumbnail.png" alt="" /><progress data-aos="fade-up" class="progress mt-3 progress-primary border h-3 ml-3 w-64" value="85" max="100"></progress>
                         </div>
                     </div>
                     <div className='ml-5 flex justify-evenly flex-col lg:flex-row sm:flex-row'>
-                        <div   className='flex my-2'>
+                        <div className='flex my-2'>
                             <img data-aos="fade-up" width={30} src="https://i.ibb.co/5KH28Qq/919832.png" alt="" /><progress data-aos="fade-up" class="progress mt-3 progress-primary border h-3 ml-3 w-64" value="10" max="100"></progress>
                         </div>
                         <div data-os="zoom-in" className='flex my-2'>
