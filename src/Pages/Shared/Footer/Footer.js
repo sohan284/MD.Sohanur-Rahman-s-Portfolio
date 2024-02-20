6f6fd4806c72c2ef511646e6f341882af264cb00
srsohan <se.fe3@ekkbaz.com>
2024-02-20 17:52:38 +0600

    add footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => {
             <div data-aos="zoom-in" className='flex items-center pt-5  justify-evenly lg:flex-row md:flex-row flex-col w-full'>
                 <div  className='text-accent '>
                 <h1 className='font-bold text-3xl'>MD.Sohanur Rahman Sohan</h1>
-                    <h1 className='font-semibold text-lg'>Junior Web Developer</h1>
+                    <h1 className='font-semibold text-lg'>Junior Front End Engineer</h1>
                 </div>
                 <div className='flex  flex-col justify-center'>
                   
                     <div className='flex items-center'>
           <img width={35} src="https://i.ibb.co/5WR76z2/pngwing-com.png" alt="" />
-          <p className='text-primary ml-2 text-lg font-bold'>+01788175088</p>
+          <p className='text-primary ml-2 text-lg font-bold'>+8801788175088</p>
                     </div>
                     <div className='flex justify-between mt-3 items-center'>
           <img width={35} src="https://i.ibb.co/LvPDKrb/pngwing-com-5.png" alt="" />
@@ -21,7 +21,7 @@ const Footer = () => {
                     </div>
                     <div className='flex justify-between mt-3 items-center'>
           <img width={35} src="https://i.ibb.co/G3wLQMv/pngwing-com-6.png" alt="" />
-          <p className='text-primary ml-2 text-lg font-bold'>Mirpur,Dhaka,Bangladesh</p>
+          <p className='text-primary ml-2 text-lg font-bold'>Khilgaon,Dhaka,Bangladesh</p>
                     </div>
                 </div>
             </div>
